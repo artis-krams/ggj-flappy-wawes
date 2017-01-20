8e5a7f20bac0eda44b2a1e9f269f7d5b2471d528
janis skuja <janis.skuja91@gmail.com>
2017-01-20 21:00:45 +0200

    fallowing camara and spawner

--- Wave/Assets/Scripts/WaveTest.cs
+++ Wave/Assets/Scripts/WaveTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,33 +11,44 @@ public class WaveTest : MonoBehaviour {
 	public float waveincrease;
 	public float currentwavestrenght;
 	public int pickupcount;
+	private int i;
 	public GameObject[] pickuplist;
 	public GameObject pickup1;
 	public GameObject pickup2;
 	public GameObject pickup3;
+	public GameObject camera;
+	public GameObject spawner;
+	public GameObject destroyer;
 	// Use this for initialization
 	void Start () {
 
 		pickuplist = new GameObject[pickupcount];
 
-
-		pickuplist[0] = Instantiate (pickup1, new Vector3(0,0,0), Quaternion.identity);
+		while (i < pickupcount) {
+			pickuplist [i] = Instantiate (pickup1, new Vector3 (spawner.transform.position.x, 0, 0), Quaternion.identity);
+			i++;
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		
+	}
 
+	void FixedUpdate() {
+
+		camera.transform.position = new Vector3 (player.transform.position.x, 0, -10);
+		//camera.transform.position.y = player.transform.position.y;
+
+	
 		player.transform.Translate (new Vector3 (speed, 0, 0));
 
-
-
-		while (Input.GetKeyDown("space")==true) //risky iet uz hang
+		if (Input.GetKeyDown("space")==true) //risky iet uz hang
 
 		{
 			print ("space pressed");
 			currentwavestrenght = currentwavestrenght + waveincrease;
 		}
-		
-		
+
 	}
 }
