1a27726f3e3bf3138aeaf373b3a18b34f686fb4b
aferox <artis.krams@gmail.com>
2017-01-20 23:43:40 +0200

    materials

---
+++ Wave/Assets/Scripts/Spinner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+    private ParticleSystem particles;
+
+    // Use this for initialization
+    void Start () {
+        particles = this.gameObject.GetComponentInChildren<ParticleSystem>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        gameObject.transform.Rotate(Vector3.up * Time.deltaTime, 1);
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            particles.Play();
+            Destroy(gameObject, 5);
+        }
+    }
+}
