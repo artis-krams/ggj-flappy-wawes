68163915f19daf437aac70a819c9579b04ad5c99
janis skuja <janis.skuja91@gmail.com>
2017-01-20 22:03:15 +0200

    SIN movement

--- Wave/Assets/Scripts/WaveTest.cs
+++ Wave/Assets/Scripts/WaveTest.cs
@@ -10,6 +10,8 @@ public class WaveTest : MonoBehaviour {
 	public float score;
 	public float waveincrease;
 	public float currentwavestrenght;
+	public float frequency;  // Speed of sine movement
+	public float magnitude;   // Size of sine movement
 	public int pickupcount;
 	private int i;
 	public GameObject[] pickuplist;
@@ -20,13 +22,17 @@ public class WaveTest : MonoBehaviour {
 	public GameObject spawner;
 	public GameObject destroyer;
 	private int spaceDown = 0;
+	private Vector3 axis;
+	private Vector3 pos;
 	// Use this for initialization
 	void Start () {
 
+		pos = player.transform.position;
+		axis = player.transform.up;
 		pickuplist = new GameObject[pickupcount];
 
 		while (i < pickupcount) {
-			pickuplist [i] = Instantiate (pickup1, new Vector3 (spawner.transform.position.x, 0, 0), Quaternion.identity);
+			pickuplist [i] = Instantiate (pickup1, new Vector3 (spawner.transform.position.x, Random.Range(10.0f,-10.0f), 0), Quaternion.identity);
 			i++;
 		}
 	}
@@ -41,9 +47,11 @@ public class WaveTest : MonoBehaviour {
 		camera.transform.position = new Vector3 (player.transform.position.x, 0, -10);
 		//camera.transform.position.y = player.transform.position.y;
 
-	
-		player.transform.Translate (new Vector3 (speed, 0, 0));
+		pos += player.transform.transform.right * Time.deltaTime * speed;
+//		player.transform.Translate (new Vector3 (speed, 0, 0));
+		player.transform.position = pos + axis * Mathf.Sin (Time.time * frequency) * currentwavestrenght;
 
+//		player.transform.position = Vector3.MoveTowards(transform.position, new Vector3 (transform.position.x+1,currentwavestrenght,0), 10);
 		if (Input.GetKeyDown("space")==true) //risky iet uz hang
 
 		{
