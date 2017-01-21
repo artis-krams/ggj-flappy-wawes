d4505825c47fcbbdfd49e9f584e88baa07d1a334
aferox <artis.krams@gmail.com>
2017-01-21 17:05:52 +0200

    input fix

--- Wave/Assets/Scripts/WaveTest.cs
+++ Wave/Assets/Scripts/WaveTest.cs
@@ -3,95 +3,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveTest : MonoBehaviour {
+public class WaveTest : MonoBehaviour
+{
 
-	public GameObject player;
-	public float speed;
-	public float score;
-	public float waveincrease;
-	public float currentwavestrenght;
-	public float frequency;  // Speed of sine movement
-	public float magnitude;   // Size of sine movement
-	public float period;
-	public int pickupcount;
-	private int i;
-	public GameObject pickup1;
-	public GameObject pickup2;
-	public GameObject pickup3;
-	public GameObject camera;
-	public GameObject spawner;
-	public GameObject destroyer;
-	public GameObject wall1;
-	public GameObject wall2;
+    public GameObject player;
+    public float speed;
+    public float score;
+    public float waveincrease;
+    public float currentwavestrenght;
+    public float frequency;  // Speed of sine movement
+    public float magnitude;   // Size of sine movement
+    public float period;
+    public int pickupcount;
+    private int i;
+    public GameObject pickup1;
+    public GameObject pickup2;
+    public GameObject pickup3;
+    public GameObject camera;
+    public GameObject spawner;
+    public GameObject destroyer;
+    public GameObject wall1;
+    public GameObject wall2;
 
-	private float nextActionTime = 0.0f;
-	private int spaceDown = 0;
-	private Vector3 axis;
-	private Vector3 pos;
-	// Use this for initialization
-	void Start () {
+    private float nextActionTime = 0.0f;
+    private int spaceDown = 0;
+    private Vector3 axis;
+    private Vector3 pos;
+    // Use this for initialization
+    void Start()
+    {
 
-		pos = player.transform.position;
-		axis = player.transform.up;
-	}
+        pos = player.transform.position;
+        axis = player.transform.up;
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	void FixedUpdate() {
+    }
 
-		camera.transform.position = new Vector3 (player.transform.position.x + 2, 0, -10);
-		//camera.transform.position.y = player.transform.position.y;
+    void FixedUpdate()
+    {
 
-		pos += player.transform.transform.right * Time.deltaTime * speed;
-//		player.transform.Translate (new Vector3 (speed, 0, 0));
-		player.transform.position = pos + axis * Mathf.Sin (Time.time * frequency) * currentwavestrenght;
-		spawner.transform.position = new Vector3 (player.transform.position.x + 25, spawner.transform.position.y, 0);
-		destroyer.transform.position = new Vector3 (player.transform.position.x - 10, destroyer.transform.position.y, 0);
-		wall1.transform.position = new Vector3 (player.transform.position.x, wall1.transform.position.y, 0);
-		wall2.transform.position = new Vector3 (player.transform.position.x, wall2.transform.position.y, 0);
+        camera.transform.position = new Vector3(player.transform.position.x + 2, 0, -10);
+        //camera.transform.position.y = player.transform.position.y;
 
-		if (Time.time > nextActionTime ) {
-			nextActionTime += period;
+        pos += player.transform.transform.right * Time.deltaTime * speed;
+        //		player.transform.Translate (new Vector3 (speed, 0, 0));
+        player.transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * currentwavestrenght;
+        spawner.transform.position = new Vector3(player.transform.position.x + 25, spawner.transform.position.y, 0);
+        destroyer.transform.position = new Vector3(player.transform.position.x - 10, destroyer.transform.position.y, 0);
+        wall1.transform.position = new Vector3(player.transform.position.x, wall1.transform.position.y, 0);
+        wall2.transform.position = new Vector3(player.transform.position.x, wall2.transform.position.y, 0);
 
-			int sw = Random.Range (0, 3);
-			switch (sw) {
-			case 0:
-				Instantiate (pickup1, new Vector3 (spawner.transform.position.x, Random.Range (8.0f, -8.0f), 0), Quaternion.identity);
-				break;
-			case 1:
-				Instantiate (pickup2, new Vector3 (spawner.transform.position.x, Random.Range (8.0f, -8.0f), 0), Quaternion.identity);
-				break;
-			case 2:
-				Instantiate (pickup3, new Vector3 (spawner.transform.position.x, Random.Range (8.0f, -8.0f), 0), Quaternion.identity);
-				break;
-			}
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
 
-		}
-//		player.transform.position = Vector3.MoveTowards(transform.position, new Vector3 (transform.position.x+1,currentwavestrenght,0), 10);
-		if (Input.GetKeyDown("space")==true) //risky iet uz hang
+            int sw = Random.Range(0, 3);
+            switch (sw)
+            {
+                case 0:
+                    Instantiate(pickup1, new Vector3(spawner.transform.position.x, Random.Range(8.0f, -8.0f), 0), Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(pickup2, new Vector3(spawner.transform.position.x, Random.Range(8.0f, -8.0f), 0), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(pickup3, new Vector3(spawner.transform.position.x, Random.Range(8.0f, -8.0f), 0), Quaternion.identity);
+                    break;
+            }
 
-		{
-			spaceDown = 1;
-		}
+        }
 
-		if (Input.GetKeyUp ("space") == true) {
-			spaceDown = 0;
-		}
-
-		if (spaceDown == 1) {
-			currentwavestrenght = currentwavestrenght + waveincrease;
-		}
-
-		if (spaceDown == 0) {
-			if (currentwavestrenght > 0) {
-				currentwavestrenght = currentwavestrenght - (3 * waveincrease);
-			} else {
-				currentwavestrenght = 0;
-			}
-		}
-
-	}
+        if (Input.GetKey("space") == true)
+        {
+            currentwavestrenght = currentwavestrenght + waveincrease;
+        }
+        else
+        {
+            if (currentwavestrenght > 0)
+            {
+                currentwavestrenght = currentwavestrenght - (3 * waveincrease);
+            }
+            else
+            {
+                currentwavestrenght = 0;
+            }
+        }
+    }
 }
