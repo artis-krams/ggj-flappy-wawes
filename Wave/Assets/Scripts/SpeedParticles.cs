f87faf07584d7bd15ad9cd83bc1b21a4487416a1
aferox <artis.krams@gmail.com>
2017-01-22 00:21:03 +0200

    speed particles

---
+++ Wave/Assets/Scripts/SpeedParticles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedParticles : MonoBehaviour {
+    private WaveTest manager;
+    ParticleSystem.VelocityOverLifetimeModule velocity;
+	void Start () {
+        var particles = GetComponent<ParticleSystem>();
+        velocity = particles.velocityOverLifetime;
+
+        manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<WaveTest>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        velocity.x = -3 - (manager.speed/4);
+	}
+}
