ae9fb70ef0f76adfd77babf5aca67cb808ab1205
aferox <artis.krams@gmail.com>
2017-01-21 18:09:22 +0200

    skybox added

---
+++ Wave/Assets/Scripts/SkyboxRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour {
+    public float rotationAmmount;
+    public Material skyboxMat;
+
+    // Use this for initialization
+    void Start () {
+        RenderSettings.skybox = skyboxMat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotationAmmount);
+    }
+}
